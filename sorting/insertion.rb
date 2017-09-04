70957f48b0df4e49f3db67dc01a85ea5ce5b6bd6
John_Hayes-Reed <john.hayes.reed@gmail.com>
2017-09-04 19:16:04 +0900

    Added Insertion Sort to collection of sorting algorithms.

---
+++ sorting/insertion.rb
@@ -0,0 +1,46 @@
+module Sorting
+  # @abstract a Service that executes an Insertion Sort on any given list. Each
+  #   item in the list must be comparable.
+  class Insertion
+    # Iterates over a list, on each iteration, insertion sort removes an element
+    #   from the input data, finds the location it belongs to within the sorted
+    #   list and inserts it there.
+    # @see https://en.wikipedia.org/wiki/Insertion_sort
+    # @example using Integers
+    #   Sorting::Insertion.call [5, 3, 7, 3, 1, 4]
+    #   #=> [1, 3, 3, 4, 5, 7]
+    # @example Using Strings.
+    #   Sorting::Insertion.call ['b', 'e', 'a', 'a', 'a', 'f']
+    #   #=> ['a', 'a', 'a', 'b', 'e', 'f']
+    # @example Using custom comparable class instances.
+    #   class TestCompare
+    #     include Comparable
+    #     attr_accessor :comp_val
+    #
+    #     def initialize(val)
+    #       @comp_val = val
+    #     end
+    #
+    #     def <=>(other)
+    #       comp_cal <=> other.comp_val
+    #     end
+    #   end
+    #
+    #   Sorting::Insertion.call [TestCompare.new(4),
+    #                            TestCompare.new(2),
+    #                            TestCompare.new(3)]
+    #   #=> [#<TestCompa...@comp_val=2>, #<Te...@comp_val=3>, #<...@comp_val=4>]
+    # @param list [Array] an unsorted list.
+    # @return [Array]
+    def self.call(list)
+      i = 1
+      while i < list.length
+        x = list[i]
+        j = i - 1
+        (list[j + 1] = list[j]) && j -= 1 while j >= 0 && list[j] > x
+        (list[j + 1] = x) && i += 1
+      end
+      list
+    end
+  end
+end
